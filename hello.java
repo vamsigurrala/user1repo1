3479d23ed209b1dab3aca3fa2ed636eb36414c08
praveen <praveengurrala1996@gmail.com>
2022-09-14 00:14:28 +0530

    hello file is commited by praveen dev2

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
-hello every one
+hello every one        #from dev2
 {
-  public static void main()	
+  public static void main()	#from dev2
   {
   system.out.println ("hello to modified by de1")
   }
-}
+}#from dev2
